9d3393c953e45c3085866fdd9c0ea5eeea0407ff
kahoor <amirr137@yahoo.com>
2020-06-29 19:44:03 +0430

    initial commit

---
+++ kernel/accounts/admin.py
@@ -0,0 +1,17 @@
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+# from .models import User
+# # Register your models here.
+
+
+# class UserAdmin(BaseUserAdmin):
+    
+#     add_fieldsets = (
+#         (None, {
+#             'classes': ('wide',),
+#             'fields': ('email', 'username','first_name', 'last_name', 'role', 'password')}
+#         ),
+#     )
+
+# admin.site.register(User, UserAdmin)
---
+++ kernel/accounts/models.py
@@ -0,0 +1,12 @@
+
+from django.db import models
+
+# Create your models here.
+
+    # ROLES_CHOICES = (
+    #     ('SA', 'Site Admin'),
+    #     ('EE', 'Employee'),
+    #     ('EO', 'Employee of an organization'),
+    #     ('DO', 'Director of an organization'),
+    # )
+
